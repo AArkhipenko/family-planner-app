af689710cccd463c0c9ded7220b0cf1f296b1c96
Arkhipenko Alex <arkhipenko.alex95@gmail.com>
2022-09-18 13:47:02 +0300

    feat(prettier): configuring prettier & change code format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'always',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    jsxSingleQuote: false,
+    endOfLine: 'lf',
 };
