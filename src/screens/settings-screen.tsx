5e571796685d06a1c9644949fa5a2c54bdcc3540
Arkhipenko Alex <arkhipenko.alex95@gmail.com>
2022-09-18 17:30:03 +0300

    feat(navigation): create navigation menu

---
+++ src/screens/settings-screen.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Button, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScreenProps } from './types';
+
+const SettingsScreen: React.FC<ScreenProps> = (_props) => {
+    const [counter, setCounter] = useState<number>(0);
+    return (
+        <SafeAreaView>
+            <Text>Press count: {counter}</Text>
+            <Button title="Press me!" onPress={() => setCounter(counter + 1)} />
+        </SafeAreaView>
+    );
+};
+
+export default SettingsScreen;
